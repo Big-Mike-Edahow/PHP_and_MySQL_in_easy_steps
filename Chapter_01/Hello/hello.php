5769650d1fcf469ec2b6b77d78534fda8d2c93cc
Mike Jackson <perigran_falcon@yahoo.com>
2025-12-03 15:38:44 -0700

    Add files via upload

---
+++ Chapter_01/Hello/hello.php
@@ -0,0 +1,27 @@
+<!-- hello.php -->
+<!-- Hello World -->
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="./favicon.ico" type="image/png" rel="shortcut icon">
+    <link href="./style.css" type="text/css" rel="stylesheet">
+    <title>Getting Started With PHP</title>
+</head>
+
+<body>
+    <main>
+        <section class="center">
+            <?php
+            # Write the traditional greeting.
+            echo '<h1 class="headline-h1">Hello World! 🌎</h1>';
+            ?>
+        </section>
+    </main>
+</body>
+
+</html>
+
